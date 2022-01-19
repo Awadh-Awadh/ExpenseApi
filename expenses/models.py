d82417c53f41c338b7ca8137b9079a292787211c
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-01-19 22:10:46 +0300

    add a model for the expenses

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,22 @@
+from turtle import ondrag
 from django.db import models
-
+from authentication.models import CustomUser
 # Create your models here.
+
+
+class Expenses(models.Model):
+
+    CATEGOIES_EXPENSE = (
+
+      ("ONLINE", "ONLINE"),
+      ("TRAVE", "TRAVEL"),
+      ("EXECUTIVE", "EXECUTIVE"),
+      ("PREMIUM", "PREMIUM"),
+      ("BASIC", "BASIC"),
+      ("MIDDLE", "MIDDLE")
+    )
+    categories = models.CharField(choices=CATEGOIES_EXPENSE, max_length=255)
+    amount=models.DecimalField(decimal_places=4, max_digits=5)
+    owner = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    description = models.TextField()
+    date=models.DateField(blank=False, null=False)
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
 
+from .models import Expenses
+from .serializers import ExpensesSerializer
 # Create your views here.
+
+
+class ExpensesListAPIView(generics.ListCreateAPIView):
+    serializer_class = ExpensesSerializer
+    permissions_classes= (permissions.IsAuthenticated,)
+
+
+
+    def get_queryset(self):
+        return Expenses.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
