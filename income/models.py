bb72a626ea111fd524b248e248bf69b9fb133943
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-01-21 14:41:17 +0300

    add income app

---
+++ income/models.py
@@ -0,0 +1,24 @@
+from pydoc import describe
+from django.db import models
+from authentication.models import CustomUser
+
+class Income(models.Model):
+
+  CATEGORIES = (
+
+    ("SALARY", "SALARY"),
+    ("SIDE_HUSTLE", "SIDE_HUSTLE"),
+    ("COMMISIONS", "COMMISIONS"),
+    ("OTHE", "OTHE")
+
+  )
+
+  categories = models.CharField(choices=CATEGORIES, max_length=255)
+  owner=models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+  amount = models.DecimalField(max_digits=7, decimal_places=2)
+  description = models.TextField()
+  date = models.DateField(blank=False, null=True)
+
+
+  def __str__(self):
+    return f"{str(self.owner)}'s Income"
