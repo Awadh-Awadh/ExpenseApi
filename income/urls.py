c1067495cfa6eaa171757f5cb2c5247fc2130c33
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-01-21 15:10:20 +0300

    add url views

--- income/urls.py
+++ income/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from . import views
 
 
+app_name="income"
 urlpatterns = [
-    
+    path("", views.IncomeAPIView.as_view(), name='income'),
+    path("<int:pk>", views.IncomeDetailAPIView.as_view(), name='detail'),
+
+
 ]
