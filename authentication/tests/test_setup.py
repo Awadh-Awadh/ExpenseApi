a1f752e48634e825ae5f63f6de25dd847f6fab86
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-01-23 01:36:46 +0300

    add testing package

---
+++ authentication/tests/test_setup.py
@@ -0,0 +1,24 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from faker import Faker
+
+
+class TestSetup(APITestCase):
+    """
+    Using Faker module to auto generate names
+    """
+
+    def setUp(self):
+        self.register = reverse('register')
+        self.login = reverse('login')
+        self.fake = Faker()
+
+        self.user_data = {
+          "email": self.fake.email(),
+          "username": self.fake.email.split('@')[0],
+          "password": self.fake.email(),
+        }
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
