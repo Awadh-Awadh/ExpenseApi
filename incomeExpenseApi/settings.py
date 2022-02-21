6a536852988f7dd16c594b2c9aa968f606af29e8
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-02-21 12:35:08 +0300

    add exception handling

--- incomeExpenseApi/settings.py
+++ incomeExpenseApi/settings.py
@@ -118,6 +118,8 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'EXCEPTION_HANDLER': 'Utils.exceptions.custom_exception_handler',
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
